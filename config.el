4f6f6b8c1fe5123cbc61a5b2ff563f385e7c7ff2
Chirurgus <oleksandr.soroch@gmail.com>
2025-01-03 07:47:16 +0100

    Start org-files folded

--- config.el
+++ config.el
@@ -56,6 +56,7 @@
   (modify-syntax-entry ?< "." org-mode-syntax-table)
   (modify-syntax-entry ?> "." org-mode-syntax-table))
 (add-hook 'org-mode-hook #'org-syntax-table-modify)
+(setq org-startup-folded t)
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
